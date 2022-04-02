14128bfbbfef51fee79958357172063a7554dd78
Dewey Ou <oushihao97@gmail.com>
2022-04-02 14:18:47 +0800

    chore: update dev

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -1,24 +1,24 @@
 module.exports = {
   overrides: [
     {
-      files: ["*.vue"],
-      parser: "vue-eslint-parser",
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
       parserOptions: {
-        parser: "@typescript-eslint/parser",
+        parser: '@typescript-eslint/parser',
       },
       rules: {
-        "no-unused-vars": "off",
-        "no-undef": "off",
-        "@typescript-eslint/no-unused-vars": "off",
+        'no-unused-vars': 'off',
+        'no-undef': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
       },
     },
   ],
-  extends: ["plugin:vue/vue3-recommended", "@ouduidui/eslint-config-ts"],
+  extends: ['plugin:vue/vue3-recommended', '@ouduidui/eslint-config-ts'],
   rules: {
-    "vue/max-attributes-per-line": "off",
-    "vue/no-v-html": "off",
-    "vue/require-prop-types": "off",
-    "vue/require-default-prop": "off",
-    "vue/multi-word-component-names": "off",
+    'vue/max-attributes-per-line': 'off',
+    'vue/no-v-html': 'off',
+    'vue/require-prop-types': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/multi-word-component-names': 'off',
   },
-};
+}
